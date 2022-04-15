9ef355146fbbf6d8eb2a73295aaa0d370ca235f9
Vlad <76936177+Vladcw@users.noreply.github.com>
2022-04-15 17:08:56 +0300

    Add files via upload

---
+++ js/images.js
@@ -0,0 +1,15 @@
+const images = {
+    "dt-1" : {
+        "name": 'Deter1',
+    },
+    "dt-2" : {
+        "name": 'Deter2',
+    },
+    "dt-3" : {
+        "name": 'Deter3',
+    },
+    "dt-4" : {
+        "name": 'Deter4',
+    },
+
+}
